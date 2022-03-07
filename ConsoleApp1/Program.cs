6b50c3d79c3867a2908c41bd2e7f4921f7dc7dd6
Petr <Petr@DESKTOP-ORMRG3E>
2022-03-07 18:17:31 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static string napsano;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("penis");
+            napsano = Console.ReadLine();
+            Console.WriteLine("délka: " + napsano.Length);
+            Console.ReadLine();
+        }
+    }
+}
